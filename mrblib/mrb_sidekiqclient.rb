93870609d6193dcf1310443a2a200ffe0efc9be0
monochromegane <dev.kuro.obi@gmail.com>
2016-01-30 12:38:14 +0900

    Removed unnecessary implementation.

--- mrblib/mrb_sidekiqclient.rb
+++ mrblib/mrb_sidekiqclient.rb
@@ -1,5 +1,2 @@
 class SidekiqClient
-  def bye
-    self.hello + " bye"
-  end
 end
--- test/mrb_sidekiqclient.rb
+++ test/mrb_sidekiqclient.rb
@@ -1,17 +1,3 @@
 ##
 ## SidekiqClient Test
 ##
-
-assert("SidekiqClient#hello") do
-  t = SidekiqClient.new "hello"
-  assert_equal("hello", t.hello)
-end
-
-assert("SidekiqClient#bye") do
-  t = SidekiqClient.new "hello"
-  assert_equal("hello bye", t.bye)
-end
-
-assert("SidekiqClient.hi") do
-  assert_equal("hi!!", SidekiqClient.hi)
-end
