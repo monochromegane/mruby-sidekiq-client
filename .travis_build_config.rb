071c95ddcd587f4c2adaf4221637bd38ee595a36
monochromegane <dev.kuro.obi@gmail.com>
2016-01-30 21:40:03 +0900

    Added mruby-secure-random gem for travis.

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -2,5 +2,6 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
   conf.gem '../mruby-sidekiq-client'
+  conf.gem :git => 'https://github.com/monochromegane/mruby-secure-random.git'
   conf.enable_test
 end
